f6adc99628af364d746fe32d21e97e6617b0a4fc
Bogdan Balanyk <balanykb@gmail.com>
2017-07-11 16:25:31 +0300

    fix year show in current date

--- WeatherForecast/Models/OpenWeatherMapModels/OpenWeatherDetailedInfo.cs
+++ WeatherForecast/Models/OpenWeatherMapModels/OpenWeatherDetailedInfo.cs
@@ -103,7 +103,7 @@ namespace WeatherForecast.Models.OpenWeatherMapModels
 
         private string UnixTimeStampToDateTime(double unixTimeStamp)
         {
-            var dtDateTime = new DateTime();
+            var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
             dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
             return dtDateTime.ToString(CultureInfo.InvariantCulture);
         }
