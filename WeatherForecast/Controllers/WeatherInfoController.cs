9a39761565bcf30f01eff4eb41ba2bcf605bf4cf
Bogdan Balanyk <balanykb@gmail.com>
2017-07-20 19:12:00 +0300

    change views

--- WeatherForecast/Controllers/WeatherInfoController.cs
+++ WeatherForecast/Controllers/WeatherInfoController.cs
@@ -44,19 +44,5 @@ namespace WeatherForecast.Controllers
             }
             return View(_detailedInfo);
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
