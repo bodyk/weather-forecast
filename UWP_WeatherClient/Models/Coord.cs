4d4287ba186cd3ebd11c037668cc2c7a9f259c9a
Bogdan Balanyk <balanykb@gmail.com>
2017-07-28 15:28:07 +0300

    add uwp weather client project

---
+++ UWP_WeatherClient/Models/Coord.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherForecast.Models.OpenWeatherMapModels
+{
+    public class Coord
+    {
+        /// <summary>
+        ///     City geo location, longitude
+        /// </summary>
+        public double lon { get; set; }
+
+        /// <summary>
+        ///     City geo location, latitude
+        /// </summary>
+        public double lat { get; set; }
+    }
+}
